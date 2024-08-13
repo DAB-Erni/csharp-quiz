424a629ae5f50f47b17cc675ba507e9e4f6b5b15
Diane <diane.besmonte@betterask.erni>
2024-08-13 11:24:54 +0800

    Added calculator error handling  and test methods

--- CalculatorApp.UnitTests/CalculatorTest.cs
+++ CalculatorApp.UnitTests/CalculatorTest.cs
@@ -6,8 +6,78 @@ namespace CalculatorApp.UnitTests;
 public class CalculatorTest
 {
     [Test]
-    public void MethodUnderTest_Scenario_ExpectedBehavior()
+    // public void MethodUnderTest_Scenario_ExpectedBehavior()
+    public void Add_TwoNumbers_ReturnsCorrectSum()
     {
-        Assert.Pass();
+        double num1 = 5;
+        double num2 = 10;
+
+        var unitUnderTest = new Calculator();
+        double testResult = unitUnderTest.Add(num1, num2);
+        double expectedResult = 15;
+
+        Assert.That(testResult, Is.EqualTo(expectedResult));
     }
+
+    [Test]
+    public void Subtract_TwoNumbers_ReturnsCorrectDifference()
+    {
+        double num1 = 20;
+        double num2 = 15;
+
+        var unitUnderTest = new Calculator();
+        double testResult = unitUnderTest.Subtract(num1, num2);
+        double expectedResult = 5;
+
+        Assert.That(testResult, Is.EqualTo(expectedResult));
+    }
+
+    [Test]
+    public void Multiply_TwoNumbers_ReturnsCorrectProduct()
+    {
+        double num1 = 10;
+        double num2 = 3;
+
+        var unitUnderTest = new Calculator();
+        double testResult = unitUnderTest.Multiply(num1, num2);
+        double expectedResult = 30;
+
+        Assert.That(testResult, Is.EqualTo(expectedResult));
+    }
+
+    [Test]
+    public void Divide_TwoNumbers_ReturnsCorrectQuotient()
+    {
+        double num1 = 30;
+        double num2 = 5;
+
+        var unitUnderTest = new Calculator();
+        double testResult = unitUnderTest.Divide(num1, num2);
+        double expectedResult = 6;
+
+        Assert.That(testResult, Is.EqualTo(expectedResult));
+    }
+
+    [Test]
+    public void Divide_NumberByZero_ThrowsDivideByZeroException()
+    {
+        double num1 = 5;
+        double num2 = 0;
+
+        var unitUnderTest = new Calculator();
+
+        Assert.Throws<DivideByZeroException>(() => unitUnderTest.Divide(num1, num2));
+    }
+
+    [Test]
+    public void Operation_InvalidInput_ThrowsFormatException()
+    {
+        string num1 = "abc";
+        var num2 = 5;
+
+        var unitUnderTest = new Calculator();
+
+        Assert.Throws<FormatException>(() => unitUnderTest.Divide(double.Parse(num1), num2));
+    }
+
 }
--- CalculatorApp/Calculator.cs
+++ CalculatorApp/Calculator.cs
@@ -9,34 +9,34 @@ public class Calculator
         switch (operation)
         {
             case "add":
-                return add(num1, num2);
+                return Add(num1, num2);
             case "subtract":
-                return subtract(num1, num2);
+                return Subtract(num1, num2);
             case "multiply":
-                return multiply(num1, num2);
+                return Multiply(num1, num2);
             case "divide":
-                return divide(num1, num2);
+                return Divide(num1, num2);
             default:
                 throw new InvalidOperationException("An error occurred: The specified operation is not supported.");
         }
 
     }
 
-    public double add(double num1, double num2)
+    public double Add(double num1, double num2)
     {
         return num1 + num2;
     }
 
-    public double subtract(double num1, double num2)
+    public double Subtract(double num1, double num2)
     {
         return num1 - num2;
     }
-    public double multiply(double num1, double num2)
+    public double Multiply(double num1, double num2)
     {
         return num1 * num2;
     }
 
-    public double divide(double num1, double num2)
+    public double Divide(double num1, double num2)
     {
         if (num2 != 0)
             return num1 / num2;
